b1a1ea929d34ac828fffa6077efa29eb67a762ac
Agustin Di Benedetto <agustindibenedetto98@gmail.com>
2021-05-30 04:00:00 -0300

    loader to index

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 const URL_JSON = 'https://jsonplaceholder.typicode.com/users'
 const LIST = document.querySelector(".list")
+const LOADER = document.querySelector(".loader")
+LOADER.style.width = "100vw"
+LOADER.style.height = "100vh"
 
-async function getResponse(url){
-    await fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        data.forEach(user => {
-            const ID = user.id
-            const NAME  = user.name
-            const li = document.createElement('li')
-            li.classList.add('list__item')
-            li.innerHTML = NAME
-            li.addEventListener('click', function(){
-                window.location.href = `./user.html?id=${ID}&user=${NAME}`
-            })
-            LIST.appendChild(li)
-        });
-    })
-    .catch(err => console.log(err))
+window.onload = ()=> {
+    LOADER.style.display = "none"
+    async function getResponse(url){
+        await fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            data.forEach(user => {
+                const ID = user.id
+                const NAME  = user.name
+                const li = document.createElement('li')
+                li.classList.add('list__item')
+                li.innerHTML = NAME
+                li.addEventListener('click', function(){
+                    window.location.href = `./user.html?id=${ID}&user=${NAME}`
+                })
+                LIST.appendChild(li)
+            });
+        })
+        .catch(err => console.log(err))
+    }
+    
+    getResponse(URL_JSON)
 }
 
-getResponse(URL_JSON)
+
